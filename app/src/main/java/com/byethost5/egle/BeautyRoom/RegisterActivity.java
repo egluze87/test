a74729e06b9e2240d95a3271a331ceb8a1fa6e73
egluze87 <egle.slekiene@gmail.com>
2017-07-26 12:58:03 +0300

    Perėjimai iš Login į Register, iš Register į Login, iš Login į Search

--- app/src/main/java/com/byethost5/egle/BeautyRoom/RegisterActivity.java
+++ app/src/main/java/com/byethost5/egle/BeautyRoom/RegisterActivity.java
@@ -1,15 +1,39 @@
 package com.byethost5.egle.BeautyRoom;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.byethost5.egle.BeautyRoom.R;
 
 public class RegisterActivity extends Activity {
 
+    private EditText username;
+    private EditText password;
+    private EditText repeat_password;
+    private Button register;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        username = (EditText) findViewById(R.id.register_username_id);
+        password = (EditText) findViewById(R.id.register_password_id);
+        repeat_password = (EditText) findViewById(R.id.register_repeat_password_id);
+        register = (Button) findViewById(R.id.register_register_button_id);
+
+
+        register.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                Intent i = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(i);
+
+            }
+        });
     }
 }
